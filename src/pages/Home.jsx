c9c65e21b1366ec3d88524769959d682546817ce
ayushi3raoina <ayushi@tlu.ee>
2025-05-05 17:50:05 +0300

    adding to fav and removing from fav done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,49 +8,72 @@ const MEAL_BYID_API = import.meta.env.VITE_MEAL_BYID_API;
 const SEARCH_API = import.meta.env.VITE_SEARCH_MEAL_API;
 
 const Home = () => {
-  const [randomMeal, setRandomMeal] = useState(null);
-  const [favoriteMeals, setFavoriteMeals] = useState([]);
-  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
-  //console.log(RANDOM_API)
+    const [randomMeal, setRandomMeal] = useState(null);
+    const [favoriteMeals, setFavoriteMeals] = useState([]);
+    const [favoriteMealIds, setFavoriteMealIds] = useState([]);
 
-  useEffect(() => {
-    loadRandomMeal();
-  },[])
+    useEffect(() => {
+        loadRandomMeal();
+        
+        const savedFavorites = localStorage.getItem("favoriteMeals");
+        if (savedFavorites) {
+            const parsedFavorites = JSON.parse(savedFavorites);
+            setFavoriteMeals(parsedFavorites);
+            setFavoriteMealIds(parsedFavorites.map((meal) => meal.idMeal));
+        }
+    }, []);
 
-  const loadRandomMeal = async () => {
-    const resp = await fetch(RANDOM_API);
-    const data = await resp.json();
-    let meal = data.meals[0];
-    console.log(meal);
-    setRandomMeal(meal);
-  }
+    const loadRandomMeal = async () => {
+        const resp = await fetch(RANDOM_API);
+        const data = await resp.json();
+        let meal = data.meals[0];
+        console.log(meal);
+        setRandomMeal(meal);
+    };
 
-  const getMealById = async (id) => {
-    const resp = await fetch(MEAL_BYID_API + id);
-    const data = await resp.json();
-    let meal = data.meals[0];
-    //console.log(meal);
-    return meal;
-  }
+    const addToFavorites = (meal) => {
+   
+        if (!favoriteMealIds.includes(meal.idMeal)) {
+            const updatedFavorites = [...favoriteMeals, meal];
+            setFavoriteMeals(updatedFavorites);
+            setFavoriteMealIds([...favoriteMealIds, meal.idMeal]);
+            localStorage.setItem(
+                "favoriteMeals",
+                JSON.stringify(updatedFavorites)
+            );
+        }
+    };
 
-  
-  return (
-    <div className="store">
-    <Search/>
-    
-    <Favorites favoriteMeals={favoriteMeals} />
-    
-    <div className="meals" id="meals">
-      {randomMeal && (
-      <MealCard 
-        mealData={randomMeal}
-        isRandom={true}
-      />
-      )}
-    </div>
-    
-  </div>
-  )
-}
+    const removeFavorite = (mealId) => {
+        const updatedFavorites = favoriteMeals.filter(
+            (meal) => meal.idMeal !== mealId
+        );
+        setFavoriteMeals(updatedFavorites);
+        setFavoriteMealIds(updatedFavorites.map((meal) => meal.idMeal));
+        localStorage.setItem("favoriteMeals", JSON.stringify(updatedFavorites));
+    };
 
-export default Home
+    return (
+        <div className="store">
+            <Search />
+
+            <Favorites
+                favoriteMeals={favoriteMeals}
+                removeFavorite={removeFavorite}
+            />
+
+            <div className="meals" id="meals">
+                {randomMeal && (
+                    <MealCard
+                        mealData={randomMeal}
+                        isRandom={true}
+                        isFavorite={favoriteMealIds.includes(randomMeal.idMeal)}
+                        addToFavorites={() => addToFavorites(randomMeal)}
+                    />
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Home;
